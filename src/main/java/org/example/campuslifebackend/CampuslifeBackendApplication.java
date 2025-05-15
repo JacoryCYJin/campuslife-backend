badd7d0083a9bcfa25877c95be685d4f320391a3
Jacory <jacory.jin@gmail.com>
2025-05-15 12:12:32 +0800

    初始化

---
+++ src/main/java/org/example/campuslifebackend/CampuslifeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.campuslifebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuslifeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampuslifeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/campuslifebackend/CampuslifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.campuslifebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuslifeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
