2aeacc6a33a75aacbb887554a209a4f1789bac26
Jacory <jacory.jin@gmail.com>
2025-05-16 11:00:25 +0800

    更新jwt的key

--- src/main/java/org/example/campuslifebackend/utils/JwtUtils.java
+++ src/main/java/org/example/campuslifebackend/utils/JwtUtils.java
@@ -2,10 +2,11 @@ package org.example.campuslifebackend.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -15,8 +16,13 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
     // 密钥，实际应用中应该放在配置文件中
-    private static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-    
+    private final Key SECRET_KEY;
+
+    public JwtUtils(@Value("${jwt.secret}") String secret) {
+        this.SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
     // token有效期（毫秒）
     private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 1000; // 5小时
     
